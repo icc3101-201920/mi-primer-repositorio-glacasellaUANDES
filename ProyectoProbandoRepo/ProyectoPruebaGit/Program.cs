c6a09efab78d4d9a138cd2d57e5d8f81af36e204
Gianfranco <glacasella@miuandes.cl>
2019-08-08 16:59:35 -0400

    Otra linea mas

--- ProyectoProbandoRepo/ProyectoPruebaGit/Program.cs
+++ ProyectoProbandoRepo/ProyectoPruebaGit/Program.cs
@@ -9,6 +9,7 @@ namespace ProyectoPruebaGit
             Console.WriteLine("Ese es el primer proyecto que subo a github classroom");
             Console.WriteLine("Esta es la segunda linea ");
             Console.WriteLine("Esta linea la escribi en el branch prueba");
+            Console.WriteLine("Otra linea que agregue estando en el branch prueba");
         }
     }
 }
