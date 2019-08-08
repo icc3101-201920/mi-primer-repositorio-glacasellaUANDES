62f3ba7ced02ae235b1a1b113f38fb5a5059d1b1
Gianfranco <glacasella@miuandes.cl>
2019-08-08 16:24:27 -0400

    Mi primer commit en este repo

---
+++ ProyectoProbandoRepo/ProyectoPruebaGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoPruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ese es el primer proyecto que subo a github classroom");
+        }
+    }
+}
---
+++ ProyectoProbandoRepo/README.md
@@ -0,0 +1,7 @@
+# PRUEBA
+
+Este proyecto es una prueba que hice para verificar el correcto funcionamiento de github classroom.
+
+Nada especial
+
+Salu2
